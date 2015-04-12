debcd1f7283ca0fd72ac6c1b70319427c72ebb88
leoderoko <leoderoko@gmail.com>
2015-04-12 18:45:54 +0500

    Add Contest model fields

--- src/Model/Contest.php
+++ src/Model/Contest.php
@@ -2,4 +2,21 @@
 
 namespace Model;
 
-class Contest {}
+class Contest
+{
+    public $id;
+
+    public $name;
+
+    public $description;
+
+    public $owner;
+
+    public $date;
+
+    public $options;
+
+    public $judes;
+
+    public $members;
+}
